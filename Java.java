45943a18c1d13b543fc9cc991702a78a1955b3ed
Sudheer Yadav <sudheer01yadav2005@gmail.com>
2025-08-05 10:40:25 +0530

    Add files via upload

---
+++ Java.java
@@ -0,0 +1,23 @@
+class Main1{
+  public static void main(String args[])
+  {
+    System.out.println("hello ");
+  }
+}
+class Main2{
+  public static void main(String args[])
+  {
+    System.out.println("hello ");
+  }
+}
+class Main3{
+  public static void main(String args[])
+  {
+    System.out.println("hello ");
+  }
+}
+class Main{
+     public static void main(String args[]){
+       System.out.println("hello");
+      }
+ }
