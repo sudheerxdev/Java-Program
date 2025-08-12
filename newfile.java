944db0e04bb172e5fce0468b9cde89607ea7caec
Sudheer Yadav <sudheer01yadav2005@gmail.com>
2025-08-12 16:10:36 +0530

    Create newfile.java

---
+++ newfile.java
@@ -0,0 +1,26 @@
+import java.util.Scanner;
+class Addition{
+  int add(int a , int b ){
+    System.out.println(a + b );
+  }
+  int add(int a){
+    System.out.println(a + 10);
+  }
+}
+// class NewAddition extends Addition{
+//   int add(int a ){
+//     System.out.println(a + 10);
+//   }
+// }
+class Demo{
+  public static void main(String args[]){
+    int num1 , num2;
+    Scanner sc = new Scanner(System.in);
+    num1 = sc.nextInt();
+    num2 = sc.nextInt();
+    Addition ad = new Addition();
+    ad.add(num1 , num2);
+    // NewAddition ad1 = new NewAddition();
+    ad1.add(num1);
+  }
+}
