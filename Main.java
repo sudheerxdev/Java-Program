5efb99231846539d0dad7e06d015d1e2a535b5af
Sudheer Yadav <sudheer01yadav2005@gmail.com>
2025-08-14 23:14:16 +0530

    Add files via upload

--- Main.java
+++ Main.java
@@ -1,23 +1,15 @@
-class Main1{
-  public static void main(String args[])
-  {
-    System.out.println("hello ");
-  }
+//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
+// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
+public class Main {
+    public static void main(String[] args) {
+        //TIP Press <shortcut actionId="ShowIntentionActions"/> with your caret at the highlighted text
+        // to see how IntelliJ IDEA suggests fixing it.
+        System.out.printf("Hello and welcome!");
+
+        for (int i = 1; i <= 5; i++) {
+            //TIP Press <shortcut actionId="Debug"/> to start debugging your code. We have set one <icon src="AllIcons.Debugger.Db_set_breakpoint"/> breakpoint
+            // for you, but you can always add more by pressing <shortcut actionId="ToggleLineBreakpoint"/>.
+            System.out.println("i = " + i);
+        }
+    }
 }
-class Main2{
-  public static void main(String args[])
-  {
-    System.out.println("hello ");
-  }
-}
-class Main3{
-  public static void main(String args[])
-  {
-    System.out.println("hello ");
-  }
-}
-class Main{
-     public static void main(String args[]){
-       System.out.println("hello");
-      }
- }
