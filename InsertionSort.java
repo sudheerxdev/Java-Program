cbdb27a6a68f980690cee41bb49b8b7598ca2ee9
Sudheer Yadav <sudheer01yadav2005@gmail.com>
2025-08-21 08:25:15 +0530

    Add files via upload

---
+++ InsertionSort.java
@@ -0,0 +1,15 @@
+package Searching_Sorting;
+
+import java.util.Scanner;
+
+class InsertionSorting{
+    public static void main(String args[]){
+        int n;
+        Scanner sc = new Scanner(System.in);
+        n = sc.nextInt();
+        int[] arr = new int[n];
+        for (int i = 0; i < n ; i++) {
+            arr[i] = sc.nextInt();
+        }
+    }
+}
