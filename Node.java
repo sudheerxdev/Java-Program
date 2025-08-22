8897d9d6063267c6894eae1a17242dde240cf8eb
Sudheer Yadav <sudheer01yadav2005@gmail.com>
2025-08-22 21:16:34 +0530

    Add files via upload

---
+++ Node.java
@@ -0,0 +1,12 @@
+package LinkedListPractice.java.Doubly;
+
+public class Node {
+    int data ;
+    Node next ;
+    Node prev ;
+    Node(int data){
+        this.data = data;
+        this.next = null;
+        this.prev = null;
+    }
+}
