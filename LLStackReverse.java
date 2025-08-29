b3230cf49f5b524f6ae1221d3809ad96674172a5
Sudheer Yadav <sudheer01yadav2005@gmail.com>
2025-08-29 13:40:34 +0530

    Add files via upload

---
+++ LLStackReverse.java
@@ -0,0 +1,104 @@
+package DataStructure.Stack;
+
+import java.util.ArrayList;
+import java.util.Scanner;
+
+//class Node{
+//    int data ;
+//    Node next;
+//    Node(int data){
+//        this.data = data ;
+//        this.next = null;
+//    }
+//}
+class StackLinkedList4{
+    Node top ;
+    void push(int data){
+        Node newnode = new Node(data);
+        if(top == null){
+            top = newnode;
+            System.out.println("data is added ast the top ");
+        }
+        else{
+            newnode.next = top;
+            top = newnode;
+            System.out.println("data is added at the node");
+        }
+    }
+    int pop(){
+        if(top == null){
+            System.out.println("stack is empty ");
+            return 0;
+        }
+        int data = top.data;
+        top = top.next;
+        return data;
+    }
+    int peak(){
+        if(top== null){
+            System.out.println("the peak element of the stack is not in the stack ");
+            return 0;
+        }
+        else{
+            return top.data;
+        }
+    }
+    void Print(){
+        if(top == null){
+            System.out.println("there is no lelement in the stavck");
+            return;
+        }
+        else{
+            Node ptr = top;
+            while(ptr != null){
+                System.out.print(ptr.data );
+                if(ptr.next != null){
+                    System.out.print(" ->");
+                }
+                ptr = ptr.next;
+            }
+        }
+        System.out.println();
+    }
+    ArrayList<Integer> AddElement(){
+        ArrayList<Integer>temp = new ArrayList<>();
+        if(top == null){
+            System.out.println("there is no lelement in the stavck");
+            return temp ;
+        }
+        else{
+            Node ptr = top;
+            while(ptr != null){
+                temp.add(ptr.data);
+                ptr = ptr.next;
+            }
+        }
+        return temp;
+    }
+}
+class LinkedListDemo4{
+    public static void main(String[] args) {
+        StackLinkedList4 obj = new StackLinkedList4();
+        int n ;
+        Scanner sc = new Scanner(System.in);
+       // System.out.println(obj.pop());
+        System.out.println("enter the number of element in the stack ");
+        n = sc.nextInt();
+        for(int i = 0 ; i < n ; i++){
+            int num ;
+            System.out.println("enter the " + (i +1) + " element in the stacck ");
+            num = sc.nextInt();
+            obj.push(num);
+        }
+//        System.out.println( " the poped item is " + obj.pop());
+//        System.out.println("the peak elemet is " + obj.peak());
+        obj.Print();
+        ArrayList<Integer> al = obj.AddElement();
+        System.out.println(al);
+        for(int i = 0 ; i < al.size() ; i++){
+            obj.push(al.get(i));
+        }
+        obj.Print();
+
+    }
+}
