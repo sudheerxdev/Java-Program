714770f6affc09062cbf9965c20935363b06c04c
Sudheer Yadav <sudheer01yadav2005@gmail.com>
2025-09-03 10:21:43 +0530

    Add files via upload

---
+++ LinkedListQueue.java
@@ -0,0 +1,54 @@
+package DataStructure.Queue;
+// normal queue implementation using linkedlist
+class Node{
+    int data ;
+    Node next;
+    Node(int data ){
+        this.data = data ;
+        this.next = null;
+    }
+
+}
+class LinkedQueue{
+    Node front ;
+    Node rear ;
+    int size ;
+    public void add(int data){
+        Node newnode = new Node(data);
+        if(front == null){
+            front = rear = newnode;
+        }
+        else{
+            rear.next = newnode;
+            rear= newnode;
+        }
+        size++;
+    }
+
+    // method of display function
+    void display(){
+        if(size == 0){
+            System.out.println("under flow ");
+            return;
+        }
+        else{
+            Node ptr = front;
+            while(ptr != null){
+                System.out.print(ptr.data + " ");
+                ptr = ptr.next;
+            }
+        }
+    }
+}
+class Demo{
+    public static void main(String[] args) {
+        LinkedQueue obj = new LinkedQueue();
+        obj.display();
+        obj.add(12);
+        obj.add(123);
+        obj.add(1234);
+        obj.add(1235);
+        obj.add(1236);
+        obj.display();
+    }
+}
