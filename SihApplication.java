cc58856c886ff5af0d549899c5c6101e89c7664f
Sudheer Yadav <sudheer01yadav2005@gmail.com>
2025-09-06 12:25:53 +0530

    Add files via upload

--- SihApplication.java
+++ SihApplication.java
@@ -5,9 +5,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class SihApplication {
-
     public static void main(String[] args) {
         SpringApplication.run(SihApplication.class, args);
     }
-
 }
