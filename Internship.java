af3d4a7ae4610c61b419f2149a5d1cceb169bc00
Sudheer Yadav <sudheer01yadav2005@gmail.com>
2025-09-06 12:27:24 +0530

    Add files via upload

--- Internship.java
+++ Internship.java
@@ -1,18 +1,12 @@
 package com.example.sih.model;
 
 import lombok.Data;
-import lombok.AllArgsConstructor;
-import lombok.NoArgsConstructor;
-import java.util.List;
 
 @Data
-@AllArgsConstructor
-@NoArgsConstructor
 public class Internship {
     private String title;
-    private String educationRequired;
-    private List<String> skillsRequired;
     private String sector;
     private String location;
-    private String duration;            // e.g. "3 months"
+    private String duration;
+    private int matchScore;
 }
