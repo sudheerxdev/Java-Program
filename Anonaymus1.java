73f5fc7218e5c9e3ff5de80c0371cd4f5e6f26bb
Sudheer Yadav <sudheer01yadav2005@gmail.com>
2025-09-18 21:54:38 +0530

    Update Anonaymus1.java

--- Anonaymus1.java
+++ Anonaymus1.java
@@ -4,10 +4,12 @@ class GoodClass{
     public static void main(String[] args) {
         Thread th = new Thread(){
             public void run(){
+                
                 System.out.println("main thread");
             }
         };
         th.start();
                 
     }
+
 }
