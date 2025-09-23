b56d9fa8a802a568fc25e98623967d5199b9274c
Sudheer Yadav <sudheer01yadav2005@gmail.com>
2025-09-23 21:57:34 +0530

    Create Practiceprogram.java

---
+++ Practiceprogram.java
@@ -0,0 +1,27 @@
+import java.util.Scanner;
+
+public class PalindromeNumberChecker {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        System.out.print("Enter a number: ");
+        int number = scanner.nextInt();
+
+        int originalNumber = number;
+        int reversedNumber = 0;
+
+        while (number != 0) {
+            int remainder = number % 10;
+            reversedNumber = reversedNumber * 10 + remainder;
+            number = number / 10;
+        }
+
+        if (originalNumber == reversedNumber) {
+            System.out.println(originalNumber + " is a palindrome.");
+        } else {
+            System.out.println(originalNumber + " is not a palindrome.");
+        }
+
+        scanner.close();
+    }
+}
