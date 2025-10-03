f8be544aa15669000e442d5755a353e94612d2b6
Sudheer Yadav <sudheer01yadav2005@gmail.com>
2025-10-03 10:40:21 +0530

    Add files via upload

---
+++ p7.java
@@ -0,0 +1,35 @@
+package corejava.threeoct;
+
+class BinarySearch {
+    public static void main(String[] args) {
+        int[] array = {2, 3, 4, 10, 40};
+        int target = 10;
+        int result = binarySearch(array, target);
+        if (result == -1) {
+            System.out.println("Element not present in array");
+        } else {
+            System.out.println("Element found at index " + result);
+        }
+    }
+
+    public static int binarySearch(int[] array, int target) {
+        int left = 0, right = array.length - 1;
+        while (left <= right) {
+            int mid = left + (right - left) / 2;
+            // Check if target is present at mid
+            if (array[mid] == target) {
+                return mid;
+            }
+            // If target greater, ignore left half
+            if (array[mid] < target) {
+                left = mid + 1;
+            }
+            // If target is smaller, ignore right half
+            else {
+                right = mid - 1;
+            }
+        }
+        // Target is not present in array
+        return -1;
+    }
+}
