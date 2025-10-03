feb7ad385de8aa62cd6bae4b5aee493c043350db
Sudheer Yadav <sudheer01yadav2005@gmail.com>
2025-10-03 10:52:38 +0530

    Add files via upload

---
+++ p21.java
@@ -0,0 +1,31 @@
+import java.util.Scanner;
+
+class CheckPrimeNumRange {
+    public static void main(String[] args) {
+
+        // 4. WAJP to print prime numbers between the range
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Enter the lower bound:");
+        int num1 = sc.nextInt();
+        System.out.println("Enter the upper bound:");
+        int num2 = sc.nextInt();
+
+        for (int j = num1; j <= num2; j++) {
+            if (isPrime(j)) {
+                System.out.println(j);
+            }
+        }
+    }
+
+    public static boolean isPrime(int num) {
+        if (num <= 1) {
+            return false;
+        }
+        for (int i = 2; i <= Math.sqrt(num); i++) {
+            if (num % i == 0) {
+                return false;
+            }
+        }
+        return true;
+    }
+}
