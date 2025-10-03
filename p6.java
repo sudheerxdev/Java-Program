65675c347816c74ed37456213720ae7081fa1ad0
Sudheer Yadav <sudheer01yadav2005@gmail.com>
2025-10-03 10:39:59 +0530

    Add files via upload

---
+++ p6.java
@@ -0,0 +1,14 @@
+package corejava.threeoct;
+
+class Stringeversal {
+    public static void main(String[] args) {
+        String original = "Hello, World!";
+        String reversed = reverseString(original);
+        System.out.println("Original String: " + original);
+        System.out.println("Reversed String: " + reversed);
+    }
+
+    public static String reverseString(String str) {
+        return reverseString(str);
+    }
+}
