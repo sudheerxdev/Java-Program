120fec2c0149ba533adda41b808cef59794e60bd
Sudheer Yadav <sudheer01yadav2005@gmail.com>
2025-10-05 11:12:04 +0530

    Add files via upload

---
+++ p82.java
@@ -0,0 +1,31 @@
+package corejava.threeoct;
+
+import java.util.Scanner;
+
+class PrimeNumberChecker {
+
+    public static boolean isPrime(int number) {
+        if (number <= 1) {
+            return false;
+        }
+        for (int i = 2; i <= Math.sqrt(number); i++) {
+            if (number % i == 0) {
+                return false;
+            }
+        }
+        return true;
+    }
+
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        System.out.print("Enter a number: ");
+        int num = scanner.nextInt();
+
+        if (isPrime(num)) {
+            System.out.println(num + " is a prime number.");
+        } else {
+            System.out.println(num + " is not a prime number.");
+        }
+        scanner.close();
+    }
+}
