ea504ed841cd7858f82eeaaf81f544980dfbe0ca
Sudheer Yadav <sudheer01yadav2005@gmail.com>
2025-10-06 18:23:09 +0530

    Add files via upload

--- p1.java
+++ p1.java
@@ -1,20 +1,30 @@
-package corejava.threeoct;
+package corejava.sixoct;
 
 import java.util.Scanner;
 
-class ContinueUntilUserSaysNo {
+class GCDCalculator {
+
+    public static int calculateGCD(int number1, int number2) {
+        while (number2 != 0) {
+            int temp = number2;
+            number2 = number1 % number2;
+            number1 = temp;
+        }
+        return number1;
+    }
 
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
-        String userInput;
 
-        do {
-            System.out.println("Doing some work...");
-            System.out.print("Do you want to continue? (Yes/No): ");
-            userInput = scanner.nextLine();
-        } while (!userInput.equalsIgnoreCase("No"));
+        System.out.print("Enter the first number: ");
+        int firstNumber = scanner.nextInt();
 
-        System.out.println("Program terminated.");
+        System.out.print("Enter the second number: ");
+        int secondNumber = scanner.nextInt();
+
+        int gcd = calculateGCD(firstNumber, secondNumber);
+        System.out.println("GCD of " + firstNumber + " and " + secondNumber + " is: " + gcd);
+
         scanner.close();
     }
 }
