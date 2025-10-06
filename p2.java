1ae9c016f2e62d9d135c33598e0c385322987074
Sudheer Yadav <sudheer01yadav2005@gmail.com>
2025-10-06 18:24:42 +0530

    Add files via upload

--- p2.java
+++ p2.java
@@ -1,26 +1,36 @@
-package corejava.threeoct;
+package corejava.sixoct;
 
 import java.util.Scanner;
 
-class SumUntilNegative {
+class LcmCalculator {
 
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
-        int sum = 0;
-        int number;
 
-        do {
-            System.out.print("Enter a number (enter a negative number to stop): ");
-            number = scanner.nextInt();
+        System.out.print("Enter the first number: ");
+        int number1 = scanner.nextInt();
 
-            if (number >= 0) {
-                sum += number;
-            }
+        System.out.print("Enter the second number: ");
+        int number2 = scanner.nextInt();
 
-        } while (number >= 0);
+        int lcm = calculateLcm(number1, number2);
 
-        System.out.println("Sum of the numbers entered (excluding the negative number): " + sum);
+        System.out.println("LCM of " + number1 + " and " + number2 + " is: " + lcm);
 
         scanner.close();
+    }
+
+    public static int calculateLcm(int num1, int num2) {
+        int gcd = calculateGcd(num1, num2);
+        return (num1 * num2) / gcd;
+    }
+
+    public static int calculateGcd(int num1, int num2) {
+        while (num2 != 0) {
+            int temp = num2;
+            num2 = num1 % num2;
+            num1 = temp;
+        }
+        return num1;
     }
 }
--- p3.java
+++ p3.java
@@ -1,27 +1,27 @@
-package corejava.threeoct;
+package corejava.sixoct;
 
 import java.util.Scanner;
 
-class PasswordValidationDoWhile {
+class ReverseNumberMethod {
+
+    public static int reverseNumber(int number) {
+        int reversedNumber = 0;
+        while (number != 0) {
+            int remainder = number % 10;
+            reversedNumber = reversedNumber * 10 + remainder;
+            number = number / 10;
+        }
+        return reversedNumber;
+    }
 
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
-        String password;
-        boolean isValid;
+        System.out.print("Enter a number: ");
+        int originalNumber = scanner.nextInt();
 
-        do {
-            System.out.print("Enter your password (at least 8 characters): ");
-            password = scanner.nextLine();
+        int reversedNumber = reverseNumber(originalNumber);
 
-            isValid = password.length() >= 8;
-
-            if (!isValid) {
-                System.out.println("Password is too short. Please try again.");
-            }
-        } while (!isValid);
-
-        System.out.println("Password accepted!");
-
+        System.out.println("Reversed number: " + reversedNumber);
         scanner.close();
     }
 }
