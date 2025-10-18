95dd8288099fcf6aea750e81d987ade1ae754481
Sudheer Yadav <sudheer01yadav2005@gmail.com>
2025-10-18 20:32:58 +0530

    Add files via upload

---
+++ RowColumnSum.java
@@ -0,0 +1,47 @@
+package corejava.oct18;
+
+import java.util.Scanner;
+
+public class RowColumnSum {
+
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        System.out.print("Enter the number of rows: ");
+        int rows = scanner.nextInt();
+
+        System.out.print("Enter the number of columns: ");
+        int cols = scanner.nextInt();
+
+        int[][] matrix = new int[rows][cols];
+
+        System.out.println("Enter the elements of the matrix:");
+        for (int i = 0; i < rows; i++) {
+            for (int j = 0; j < cols; j++) {
+                matrix[i][j] = scanner.nextInt();
+            }
+        }
+
+        // Calculate and print row sums
+        System.out.println("Row Sums:");
+        for (int i = 0; i < rows; i++) {
+            int rowSum = 0;
+            for (int j = 0; j < cols; j++) {
+                rowSum += matrix[i][j];
+            }
+            System.out.println("Row " + (i + 1) + ": " + rowSum);
+        }
+
+        // Calculate and print column sums
+        System.out.println("Column Sums:");
+        for (int j = 0; j < cols; j++) {
+            int colSum = 0;
+            for (int i = 0; i < rows; i++) {
+                colSum += matrix[i][j];
+            }
+            System.out.println("Column " + (j + 1) + ": " + colSum);
+        }
+
+        scanner.close();
+    }
+}
