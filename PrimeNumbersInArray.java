1e66468ab6cfbfde2d80b9ed00a571afe11cfd6c
Sudheer Yadav <sudheer01yadav2005@gmail.com>
2025-10-18 20:32:32 +0530

    Add files via upload

---
+++ PrimeNumbersInArray.java
@@ -0,0 +1,42 @@
+package corejava.oct18;
+
+import java.util.Scanner;
+
+public class PrimeNumbersInArray {
+
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        System.out.print("Enter the number of elements in the array: ");
+        int n = scanner.nextInt();
+
+        int[] arr = new int[n];
+
+        System.out.println("Enter the elements of the array:");
+        for (int i = 0; i < n; i++) {
+            arr[i] = scanner.nextInt();
+        }
+
+        System.out.println("Prime numbers in the array are:");
+        for (int i = 0; i < n; i++) {
+            if (isPrime(arr[i])) {
+                System.out.print(arr[i] + " ");
+            }
+        }
+        System.out.println();
+
+        scanner.close();
+    }
+
+    static boolean isPrime(int num) {
+        if (num <= 1) {
+            return false;
+        }
+        for (int i = 2; i <= Math.sqrt(num); i++) {
+            if (num % i == 0) {
+                return false;
+            }
+        }
+        return true;
+    }
+}
