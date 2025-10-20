8e149f0784e745e951fca7f611760394c339104f
Sudheer Yadav <sudheer01yadav2005@gmail.com>
2025-10-20 19:50:51 +0530

    Add GCD calculation using while loop

---
+++ Progp1.java
@@ -0,0 +1,47 @@
+package corejava;
+
+
+
+import java.util.Scanner;
+
+class GCDWhileLoop {
+
+
+
+    public static void main(String[] args) {
+
+        Scanner scanner = new Scanner(System.in);
+
+
+
+        System.out.print("Enter first number: ");
+
+        int number1 = scanner.nextInt();
+
+
+
+        System.out.print("Enter second number: ");
+
+        int number2 = scanner.nextInt();
+
+
+
+        while (number2 != 0) {
+
+            int temp = number2;
+
+            number2 = number1 % number2;
+
+            number1 = temp;
+
+        }
+
+
+
+        System.out.println("GCD is: " + number1);
+
+        scanner.close();
+
+    }
+
+}
