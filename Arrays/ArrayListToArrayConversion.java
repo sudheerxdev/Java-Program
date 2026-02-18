9a79067ab1505ef76ccdac62cf16355a766ceb20
Sudheer Yadav <sudheer01yadav2005@gmail.com>
2026-02-19 00:48:24 +0530

    Remove unnecessary whitespace in main method

--- Arrays/ArrayListToArrayConversion.java
+++ Arrays/ArrayListToArrayConversion.java
@@ -6,7 +6,6 @@ import java.util.List;
 public class ArrayListToArrayConversion {
 
 	public static void main(String[] args) {
-		
 		List<Integer> al = new ArrayList<Integer>();
 		al.add(10);
 		al.add(20);
