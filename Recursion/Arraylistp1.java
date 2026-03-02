9d94e03298863e2a74b4768cfe39f668a7035f85
Sudheer Yadav <sudheer01yadav2005@gmail.com>
2026-03-02 18:20:41 +0530

    Add files via upload

---
+++ Recursion/Arraylistp1.java
@@ -0,0 +1,13 @@
+
+class Main{
+
+public static void main(String[] args) {
+    ArrayList<String> list = new ArrayList<String>();
+    list.add("Hello");
+    list.add("World");
+    list.add("Java");
+    list.add("Programming");
+
+    System.out.println("ArrayList: " + list);
+}
+}
