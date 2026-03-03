b631a6a4da8c30ade345f4f45e89f40408723064
Sudheer Yadav <sudheer01yadav2005@gmail.com>
2026-03-03 17:18:24 +0530

    Implement user input for Armstrong number check
    Added user input functionality to check Armstrong numbers.

--- Math/Aramstrong.java
+++ Math/Aramstrong.java
@@ -3,6 +3,7 @@ import java.util.Scanner;
 public class ArmstrongNumberChecker {
 
     public static void main(String[] args) {
+        
         Scanner scanner = new Scanner(System.in);
         System.out.print("Enter a number: ");
         int number = scanner.nextInt();
