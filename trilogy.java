bd7604768a663b047679b53141ac6915b5e4e855
Sudheer Yadav <sudheer01yadav2005@gmail.com>
2026-04-22 22:13:15 +0530

    Create trilogy.java

---
+++ trilogy.java
@@ -0,0 +1,166 @@
+Dear Students,
+
+Greetings of the day!
+
+
+We are pleased to inform you that Trilogy is hiring for  B.Tech CS (All branches) & IT 2027 passing batch students.
+Job Description (Same JD for interns and full-time)
+e-ppt
+student docket 
+The registration link is attached along with the Job Description (JD). All eligible students are advised to read JD very carefully before applying and complete the Google Form by Sunday 15th February, before 5:00 PM.
+
+
+Note: It is mandatory for all eligible students to fill out the Google Form till 15th February 5:00 PM.
+
+
+Company Name:
+Trilogy
+About Company :
+Trilogy Innovations prides itself on being a software innovation factory. We are a software product development organization that resolves to solve the toughest business problems in the enterprise space. e-ppt attached for your reference and to share with all students.
+Official Website :
+https://innovations.trilogy.com
+Registration Link:
+https://forms.gle/cKQSyPvHxKmsUtQT7
+Key Responsibilities
+● Learn & Apply Fast – Master new tech & AI tools in the first month.
+● Innovate with AI – Solve real problems with AI-augmented solutions.
+● Write Smart Code – Efficient, scalable, and AI-optimized.
+● Collaborate Seamlessly – Work across teams, and adapt quickly.
+● Own the Outcome – Take full responsibility for project success.
+● Evolve Continuously – Seek, share, and act on feedback.
+Registration Link last Date:
+15th Feb 2026
+Course & Branch :
+B.Tech CS ( All branches) & IT
+Cut-off :
+70% Throughout career 
+Batch :
+2027 Passouts
+Employment Type
+Internship + Full time
+Internship Details
+Stipend: Rs 75,000 , Duration of internship: 2 months
+
+
+Salary Bifurcation (Fixed + Variable) :
+INR 32.5L PER ANNUM
+Designation 1:
+Software Development Engineer (FTE)
+Job description /Job Profile
+Software Development Engineers are expected to develop and present
+ideas on the next generation of solutions that will drive TI’s business in the future. Software Development
+Engineers immediately join TI University (TU) , TI’s power bootcamp for fresh recruits that aims at
+accelerated transformation to super problem solvers and tech professionals.
+Responsibilities:
+A Software Development Engineer will deliver components of an application on time and to specifications a key member of a product or delivery team. He or she is expected to do so by making balanced
+technical decisions (e.g., design trade-offs, prioritization, etc.) as a member of his or her team. These
+decisions must reflect knowledge of applying the appropriate technical/theoretical knowledge within the
+context of the customer’s business requirements. Supports the delivery team in deploying product functionality to customers (e.g., answers questions,
+explains functionality, extends API, provides code reviews, etc.).
+Designation 2 :
+Software Development Engineer Intern
+Intern Job Description:
+A Software Development Engineer Intern will deliver components of an
+application on time and to specification as a key member of a product or delivery team. He or she is
+expected to do so by making balanced technical decisions (e.g., design trade-offs, prioritization, etc.) as a
+member of his or her team. These decisions must reflect knowledge of applying the appropriate
+technical/theoretical knowledge within the context of the customer’s business requirements. Supports the
+delivery team in deploying product functionality to customers (e.g., answers questions, explains
+functionality, extends API, provides code reviews, etc.).
+Online Test requirements:
+Laptop with Good Internet Bandwidth
+Selection Process :
+1. Online Technical Test (Coding Test) - 120 minutes.
+2. Shortlists from the coding test will have to give an Online Cognitive Aptitude Test (CCAT) of
+15 minutes. Candidates who score 40 or above will move to interviews.
+3. Technical Interviews: 3 rounds of - 30 mins/ 1 hour each for interns.                                     TI R0 (Resume Round) 30 mins
+TI R1 ( DS + Algo) 60 mins
+TI R2 ( Design + Open-ended problem
+solving/ AI round) 60 mins                                                                                                                     4. Candidates selected after the interview process will have to take the Proctored Online Cognitive
+Aptitude Test (PCCAT) which is nothing but a proctored CCAT. Duration is 15 minutes. This
+step to be followed for Intern PPO.
+Required Skill Set/ Key Skills :
+Hands-on in coding, at least 1 scripting and 1 OOP language.
+Good problem-solving skills.
+System design basics.
+Good communication skills.
+Good Analytical Skills.
+DS Algorithms.
+AI exposure
+Campus Interview Date :
+Will inform
+Reporting Time:
+Will inform
+Interview Location :
+On Campus
+Interview Round
+5 Round
+Tentative Joining Date :
+11th May 2026
+Joining Locations :
+Will update
+Gender Preference :
+No
+Any Bond
+No
+
+Late responses will not be considered, so please ensure timely submission.
+Best Wishes,
+Shalini Dixit 
+Sr. Manager - Training & Placement 
+BBD University, Lucknow
+https://bbdu.ac.in/
+Mo. 6386099457
+ 
+Trilogy Online Test Scheduled Tomorrow for B. Tech CS ( All branches) & IT 2027 passing out batch at (12:00 PM – 1:00 PM)
+Inbox
+
+Summarise this email
+
+SHAILNI DIXIT <dixit.shalini1417@bbdu.ac.in>
+Attachments
+15:50 (3 hours ago)
+to bcc: me
+
+Dear Students,
+
+Greetings of the day!
+
+We are pleased to inform you that Trilogy has scheduled an online test for B.Tech (CS All branches) & IT 2027 passing batch students for tomorrow, Thursday, 23rd April 2026, from 12:00 PM to 1:00 PM.
+
+You will receive the test link 15 minutes prior to the test, at 11:45 AM, on your registered email ID. Kindly remain active on your email and ensure that you check it on time.
+
+Test Details – Round 1
+1. Test Type: Online Test
+2. Test Date: Thursday, 23rd April,2026 
+3. Time : 12:00 PM– 1:00 PM
+(Students are advised to complete the system check half hour before the start time.)
+4. Assessment Duration: 1 hour
+
+Important Instructions for the online test :
+   1-  It is mandatory to attend the test on your own laptop with a proper strong network connection.
+   2- This is a proctored test.
+   3- Any kind of malpractice will lead to immediate suspension of the test.
+   5-  Any such activity may lead to disqualification and blacklisting by the company, making you ineligible to apply in the future.
+   6- Attendance is mandatory for all eligible students during the test.
+
+(Students are advised to complete the system check half an hour before the start time.)
+
+ Note: You can attend the test from your home but keep in mind it's a proctored test so don't use any kind of malpractice.
+
+In case you face any issues during the test, you may connect with us for assistance.
+
+ All eligible students list is attached for your reference
+
+Best of luck to all of you.
+ 
+Best wishes,
+
+Shalini Dixit 
+Sr. Manager - Training & Placement 
+BBD University, Lucknow
+https://bbdu.ac.in/
+Mo. 6386099457
+
+
+image.png
